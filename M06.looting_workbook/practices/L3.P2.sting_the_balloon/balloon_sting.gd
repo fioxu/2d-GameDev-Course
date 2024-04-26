b1cbd4feb622219904e91c20add4865a0455df1b
fioxu <fixu@s.sfusd.edu>
2024-04-26 15:44:50 -0700

    L3 + L3.P2

--- M06.looting_workbook/practices/L3.P2.sting_the_balloon/balloon_sting.gd
+++ M06.looting_workbook/practices/L3.P2.sting_the_balloon/balloon_sting.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
-
 func _ready() -> void:
-	pass
-
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
 
 func _on_mouse_entered() -> void:
+	queue_free()
+
+func _on_mouse_exited() -> void:
 	pass
