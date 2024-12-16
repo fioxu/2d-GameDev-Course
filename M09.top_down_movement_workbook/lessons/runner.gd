cccdd2d2edd545862feed90722f1cbdc51a82029
fioxu <fixu@s.sfusd.edu>
2024-12-15 16:43:11 -0800

    M9L5 + L5P1

--- M09.top_down_movement_workbook/lessons/runner.gd
+++ M09.top_down_movement_workbook/lessons/runner.gd
@@ -1,54 +1,29 @@
 extends CharacterBody2D
 
-const RUNNER_DOWN = preload("res://assets/runner_down.png")
-const RUNNER_DOWN_RIGHT = preload("res://assets/runner_down_right.png")
-const RUNNER_RIGHT = preload("res://assets/runner_right.png")
-const RUNNER_UP = preload("res://assets/runner_up.png")
-const RUNNER_UP_RIGHT = preload("res://assets/runner_up_right.png")
+@onready var _runner_visual: RunnerVisual = %RunnerVisualRed
 
-const UP_RIGHT = Vector2.UP + Vector2.RIGHT
-const UP_LEFT = Vector2.UP + Vector2.LEFT
-const DOWN_RIGHT = Vector2.DOWN + Vector2.RIGHT
-const DOWN_LEFT = Vector2.DOWN + Vector2.LEFT
-
-#ANCHOR:max_speed
-var max_speed := 600.0
-#END:max_speed
-
-@onready var _skin: Sprite2D = %Skin
-
+@export var max_speed := 600.0
+@export var acceleration := 1200.0
+@export var deceleration := 1080.0
 
 #ANCHOR:physics_process_definition
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 #END:physics_process_definition
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-#ANCHOR:movement
-	velocity = direction * max_speed
-#END:movement
+	var has_input_direction := direction.length() > 0.0
+	if has_input_direction:
+		var desired_velocity := direction * max_speed
+		velocity = velocity.move_toward(desired_velocity, acceleration * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
 	move_and_slide()
-
-	var direction_discrete := direction.sign()
-#ANCHOR:match
-	match direction_discrete:
-#END:match
-#ANCHOR:match_right
-		Vector2.RIGHT, Vector2.LEFT:
-#END:match_right
-			_skin.texture = RUNNER_RIGHT
-		Vector2.UP:
-			_skin.texture = RUNNER_UP
-		Vector2.DOWN:
-			_skin.texture = RUNNER_DOWN
-#ANCHOR:match_up_right
-		UP_RIGHT, UP_LEFT:
-#END:match_up_right
-			_skin.texture = RUNNER_UP_RIGHT
-#ANCHOR:match_down_right
-		DOWN_RIGHT, DOWN_LEFT:
-#END:match_down_right
-			_skin.texture = RUNNER_DOWN_RIGHT
-
-#ANCHOR:flip_h
-	if direction_discrete.length() > 0:
-		_skin.flip_h = direction.x < 0.0
-#END:flip_h
+	if direction.length() > 0.0:
+		_runner_visual.angle = rotate_toward(_runner_visual.angle, direction.orthogonal().angle(), 8.0 * delta)
+		var current_speed_percent := velocity.length() / max_speed
+		_runner_visual.animation_name =(
+			RunnerVisual.Animations.WALK
+			if current_speed_percent < 0.8
+			else RunnerVisual.Animations.RUN
+		)
+	else:
+		_runner_visual.animation_name = RunnerVisual.Animations.IDLE
