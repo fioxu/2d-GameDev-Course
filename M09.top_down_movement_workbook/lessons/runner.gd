ca8538f729e73bf7b8769310b2a85fc38b2c9369
fioxu <fixu@s.sfusd.edu>
2024-12-19 21:43:04 -0800

    M9L8

--- M09.top_down_movement_workbook/lessons/runner.gd
+++ M09.top_down_movement_workbook/lessons/runner.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+class_name Runner
 
 @onready var _runner_visual: RunnerVisual = %RunnerVisualRed
 @onready var _dust: GPUParticles2D = %Dust
@@ -6,6 +7,8 @@ extends CharacterBody2D
 @export var max_speed := 600.0
 @export var acceleration := 1200.0
 @export var deceleration := 1080.0
+
+signal walked_to
 
 #ANCHOR:physics_process_definition
 func _physics_process(delta: float) -> void:
@@ -26,3 +29,18 @@ func _physics_process(delta: float) -> void:
 	else:
 		_runner_visual.animation_name = RunnerVisual.Animations.IDLE
 		_dust.emitting = false
+
+func walk_to(destination_global_position: Vector2) -> void:
+	var direction := global_position.direction_to(destination_global_position)
+	_runner_visual.angle = direction.orthogonal().angle()
+	_runner_visual.animation_name = RunnerVisual.Animations.WALK
+	_dust.emitting = true
+	var distance := global_position.distance_to(destination_global_position)
+	var duration := distance / (max_speed * 0.2)
+	var tween := create_tween()
+	tween.tween_property(self, "global_position", destination_global_position, duration)
+	tween.finished.connect(func(): 
+		_runner_visual.animation_name = RunnerVisual.Animations.IDLE
+		_dust.emitting = false
+		walked_to.emit()
+	)
